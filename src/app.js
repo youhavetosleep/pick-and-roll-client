c00b49fbaab547334fcbc2586460171c0acf86c8
youhavetosleep <qksekf4@gmail.com>
2021-09-28 20:24:02 +0900

    signup form ex-done

--- src/app.js
+++ src/app.js
@@ -8,17 +8,21 @@ import PrivateRoute from "./component/privateRoute"
 import PrivateRoute2 from "./component/privateRoute2"
 import LoginForm from './component/login'
 import Signup from "./page/signup";
+import Users from './page/users';
+import PostWrite from './page/postWrite';
 
 function App() {
   return (
     
     <AuthContextProvider>
     <Router>
-      <Navbar></Navbar>
+      <Navbar />
       <Switch>
       <Route exact path="/" component={Info} />
       <Route exact path="/login" component={LoginForm} />
       <Route exact path="/signup" component={Signup} />
+      <Route exact path="/users" component={Users} />
+      <Route exact path="/postWrite" component={PostWrite} />
       </Switch>
     </Router>
     </AuthContextProvider>
